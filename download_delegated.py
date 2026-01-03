63fb930a92181c0dfa543189e19870c107801e27
Gideon Kiprotich <36530923+kgideon781@users.noreply.github.com>
2026-01-03 19:59:19 +0300

    Update download_delegated.py

--- download_delegated.py
+++ download_delegated.py
@@ -86,8 +86,8 @@ FILES_TO_DOWNLOAD = [
         'description': 'Cohort 1-10 Demographics'
     },
     {
-        'search_terms': ['institutional participant list'],
-        'filename_contains': 'institutional participant list',
+        'search_terms': ['participant list for institutional staff trained'],
+        'filename_contains': 'participant list for institutional staff trained',
         'output_name': 'Institutionalization',
         'description': 'Institutional Achievements DB'
     },
